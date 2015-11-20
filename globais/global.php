8e63755955bd8ad7dcfdddcfb902617b8f7b8813
gilmar_vinicius <gilmar_vinicius@SNSOO1EP01VM008.saojose.sc.senai.br>
2015-11-20 14:22:02 -0200

    session funcional cookie funcional

--- globais/global.php
+++ globais/global.php
@@ -15,27 +15,13 @@
     }
     
     function valor_nome(){
-        if ($_SESSION["cont"] == 1){
-            $cookie_nome = $_POST["nome"];
-            $cookie_valor_nome = "salvar";
-            setcookie($cookie_nome, $cookie_valor_nome, time() + (86400 * 30), "/"); // 86400 = 1 dia
-        }
-        if($_SESSION["cont"] == 2 and $_SESSION["cont"] > 2){
-            echo $_COOKIE[$cookie_nome];
-            $_SESSION["cont"] = 1;
+        if(!empty($_COOKIE["nome"])){
+            echo $_COOKIE["nome"];
         }
     }
     
     function valor_senha(){
-        if ($_SESSION["cont"] == 1){
-            $cookie_senha = $_POST["senha"];
-            $cookie_value_senha = "salvar";
-            setcookie($cookie_senha, $cookie_value_senha, time() + (86400 * 30), "/");
-            echo $_COOKIE[$cookie_senha];
-        }
-        if($_SESSION["cont"] == 2){
-            echo $_COOKIE[$cookie_senha];
-            $_SESSION["cont"] = 1;
-            echo $_SESSION;
+        if(!empty($_COOKIE["senha"])){
+            echo $_COOKIE["senha"];
         }
     }
